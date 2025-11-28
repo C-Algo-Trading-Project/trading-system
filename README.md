97556dfb1530166d7b32a922e2624a071ac077f4
Refrozen <zhou.ke.t7@dc.tohoku.ac.jp>
2025-11-28 22:03:42 +0900

    Add CSV parsing functionality and update Bar structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# trading-system
+# C++ Stock Trading System
 A C++ based stock trading system.
+
+## Dependencies
+- [fast-cpp-csv-parser](https://github.com/ben-strasser/fast-cpp-csv-parser): Already included in the repository.
---
+++ src/data/csv_loader.cpp
@@ -0,0 +1,59 @@
+#include "csv.h"
+#include "data/csv_loader.h"
+
+#include <stdexcept>
+
+namespace trading
+{
+
+    std::vector<Bar> load_bars_from_csv(const std::filesystem::path &csv_path)
+    {
+        if (!std::filesystem::exists(csv_path))
+        {
+            throw std::runtime_error("CSV file not found: " + csv_path.string());
+        }
+
+        io::CSVReader<10> in(csv_path.string());
+        in.read_header(io::ignore_extra_column,
+                       "TICKER",
+                       "PER",
+                       "DATE",
+                       "TIME",
+                       "OPEN",
+                       "HIGH",
+                       "LOW",
+                       "CLOSE",
+                       "VOL",
+                       "OPENINT");
+
+        std::vector<Bar> bars;
+        std::string ticker;
+        std::string period;
+        std::string date;
+        std::string time;
+        double open{};
+        double high{};
+        double low{};
+        double close{};
+        std::uint64_t volume{};
+        std::uint64_t open_interest{};
+
+        while (in.read_row(ticker, period, date, time, open, high, low, close, volume, open_interest))
+        {
+            bars.push_back(Bar{
+                ticker,
+                period,
+                date,
+                time,
+                open,
+                high,
+                low,
+                close,
+                volume,
+                open_interest});
+        }
+
+        return bars;
+    }
+
+} // namespace trading
